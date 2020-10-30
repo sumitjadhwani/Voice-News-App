fe1526caf21e3f8b01fd2d1592e62132330985f4
sumitjadhwani <sumitjadhwani@gmail.com>
2020-10-30 13:13:20 +0530

    updated version

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+// import classes from '*.module.css';
+import useStyles from './styles.js';
+import wordsToNumbers from 'words-to-numbers';
+
+const alanKey = 'd37aeb7f2feca05aae60312818ec41b72e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
+
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles, number }) => {
+                if(command === 'newsHeadlines')
+                {
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+
+                } else if(command === 'highlight'){
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if(command === 'open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+                    const article =articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20){
+                        alanBtn().playText('Please try that again.');
+                    }else if(article){
+                        window.open(article.url,'_blank');
+                        alanBtn().playText('opening...');
+                    }
+                }
+            }
+        })
+    }, [])
+    return (
+        <div>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="alan logo"/>
+            </div>
+            <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+        </div>
+    );
+}
+
+export default App;
